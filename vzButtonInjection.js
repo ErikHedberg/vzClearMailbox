2c8487fd94ae1b5b88fc210738ac425348c33fbd
ErikHedberg <snowbelowFTW@gmail.com>
2015-07-25 19:20:15 -0400

    Create vzButtonInjection.js

---
+++ vzButtonInjection.js
@@ -0,0 +1,19 @@
+inboxViewIsActive = document.getElementById("INBOX_Emails") ? document.getElementById("INBOX_Emails").className == "active" : true;
+if(!document.getElementById("deleteAll")) {
+	divMain = document.createElement("div");
+	divMain.setAttribute("id","deleteAll");
+	divMain.setAttribute("class","CH MarL2 fl");
+	divMain.setAttribute("title","Delete All");
+	divMain.setAttribute("onClick","var deb = false function deletePage() {if(!deb && document.getElementById('INBOX_Emails').className) {deb = true;document.getElementById('selector').click();document.getElementById('delButton').click();document.getElementsByClassName('BtnRed')[2].click();}else{deb=false;return;}}sleepTime = 500;numPages = parseInt(document.getElementsByClassName('bold fl')[1].firstChild.nodeValue);interval = setInterval(function() {deletePage()},sleepTime);setTimeout(function() {deb=false;clearInterval(interval)},sleepTime*(2*numPages));");
+
+	divMain.innerHTML = "<div class='btnBgL fl'></div> <div class='btnBg fl' style='width: 80px'><b>Delete All</b></div> <div class='btnBgR fl'></div>";
+	
+	delButton = document.getElementById("delButton");
+	
+	if(delButton) {
+		delButton.parentNode.appendChild(divMain);
+	};
+}else if(document.getElementById("deleteAll") && !inboxViewIsActive) {
+	divMain = document.getElementById("deleteAll");
+	divMain.parentNode.removeChild(divMain);
+};
