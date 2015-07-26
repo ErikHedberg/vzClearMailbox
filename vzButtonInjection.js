a15a51e8549a34d3262aff495dc0efc54abae74f
Erik Hedberg <snowbelowFTW@gmail.com>
2015-07-26 10:58:23 -0400

    vzButtonInjection.js

--- vzButtonInjection.js
+++ vzButtonInjection.js
@@ -4,7 +4,7 @@ if(!document.getElementById("deleteAll")) {
 	divMain.setAttribute("id","deleteAll");
 	divMain.setAttribute("class","CH MarL2 fl");
 	divMain.setAttribute("title","Delete All");
-	divMain.setAttribute("onClick","var deb = false function deletePage() {if(!deb && document.getElementById('INBOX_Emails').className) {deb = true;document.getElementById('selector').click();document.getElementById('delButton').click();document.getElementsByClassName('BtnRed')[2].click();}else{deb=false;return;}}sleepTime = 500;numPages = parseInt(document.getElementsByClassName('bold fl')[1].firstChild.nodeValue);interval = setInterval(function() {deletePage()},sleepTime);setTimeout(function() {deb=false;clearInterval(interval)},sleepTime*(2*numPages));");
+	divMain.setAttribute("onClick","function deletePage() {if(document.getElementById('INBOX_Emails').className == 'active') {document.getElementById('selector').click();document.getElementById('delButton').click();document.getElementsByClassName('BtnRed')[2].click();}else{return;}}sleepTime = 500;numPages = parseInt(document.getElementsByClassName('bold fl')[1].firstChild.nodeValue);interval = setInterval(function() {deletePage()},sleepTime);setTimeout(function() {deb=false;clearInterval(interval)},sleepTime*(2*numPages));");
 
 	divMain.innerHTML = "<div class='btnBgL fl'></div> <div class='btnBg fl' style='width: 80px'><b>Delete All</b></div> <div class='btnBgR fl'></div>";
 	
